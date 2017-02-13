8d2062f439a97719a8ae50992f58633422a2ac01
Apple <2455502155@qq.com>
2017-02-13 17:57:38 +0800

    no message

---
+++ audioEngineTest/audioEngineTest/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  audioEngineTest
+//
+//  Created by luckyncl on 17/2/13.
+//  Copyright © 2017年 luckyncl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <TheAmazingAudioEngine/TheAmazingAudioEngine.h>
+#import "AEAudioController.h"
+@interface ViewController : UIViewController
+
+@property (nonatomic, strong) AEAudioController *audio;
+@end
+
